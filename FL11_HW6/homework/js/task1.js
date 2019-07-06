114b477b66c6336fde3e118578f5985067380921
maryocean <marry.pavliuk@gmail.com>
2019-07-06 20:10:32 +0300

    no magic

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -4,7 +4,8 @@ const b1 = parseInt(prompt('Please, enter b1', ''));
 const b2 = parseInt(prompt('Please, enter b2', ''));
 const c1 = parseInt(prompt('Please, enter c1', ''));
 const c2 = parseInt(prompt('Please, enter c2', ''));
-if (c1 === (a1 + b1) / 2 && c2 === (a2 + b2) / 2) {
+const two = 2;
+if (c1 === (a1 + b1) / two && c2 === (a2 + b2) / two) {
     console.log(true);
 } else {
     console.log(false);
