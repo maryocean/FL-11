143daa85712ec63049070011bc85eb0d0f44532e
maryocean <marry.pavliuk@gmail.com>
2019-07-06 20:21:51 +0300

    Refactor

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -4,11 +4,13 @@ let adminEmail = 'admin@gmail.com';
 let adminPassword = 'AdminPass';
 
 let password;
+const minEmailLength = 6;
+const minPasswordLength = 5;
 
 let visitorEmail = prompt('Please, enter your email', '');
-if (visitorEmail == '' || visitorEmail == null) {
+if (visitorEmail === '' || visitorEmail === null) {
     alert('Canceled');
-} else if (visitorEmail.length < 6) {
+} else if (visitorEmail.length < minEmailLength) {
     alert("I don't know any emails having name length less than 6 symbols");
 } else if (visitorEmail === userEmail || visitorEmail === adminEmail) {
     if (visitorEmail === userEmail) {
@@ -17,17 +19,17 @@ if (visitorEmail == '' || visitorEmail == null) {
         password = adminPassword;
     }
     let visitorPassword = prompt('Please, enter your password', '');
-    if (visitorPassword == password) {
-        let change = confirm("Do you want to change your password?");
-        if (change == false) {
+    if (visitorPassword === password) {
+        let change = confirm('Do you want to change your password?');
+        if (change === false) {
             alert('You have failed the change.');
-        } else if (change == true) {
+        } else if (change === true) {
             let visitorPassword = prompt('Please, enter your old password', '');
-            if (visitorPassword == '' || visitorPassword == null) {
+            if (visitorPassword === '' || visitorPassword === null) {
                 alert('Canceled');
-            } else if (visitorPassword == password) {
+            } else if (visitorPassword === password) {
                 let visitorNewPassword = prompt('Please, enter your new password', '');
-                if (visitorNewPassword.length < 5) {
+                if (visitorNewPassword.length < minPasswordLength) {
                     alert('It’s too short password. Sorry.');
                 } else {
                     let visitorEnterNewPassword = prompt('Please, enter your new password again', '');
@@ -41,12 +43,12 @@ if (visitorEmail == '' || visitorEmail == null) {
                 alert('Wrong password');
             }
         }
-    } else if (visitorPassword == '' || visitorPassword == null) {
+    } else if (visitorPassword === '' || visitorPassword === null) {
         alert('Canceled');
     } else {
         alert('Wrong password');
     }
 } else {
-    alert("I don’t know you");
+    alert('I don’t know you');
 }
 
