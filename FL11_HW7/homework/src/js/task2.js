a1dde161153e6cf85f41932c3dd869aede900497
maryocean <marry.pavliuk@gmail.com>
2019-07-09 21:13:14 +0300

    Refactor

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,55 +1,56 @@
-let wantsToPlay;
+// to reset game state
+let startMax = 5;
+let startUserPrize = 0;
+let startBaseMaxPrize = 100;
+// game properties
 let min = 0;
-let max = 5;
+let max = startMax;
+let step = 4;
 let random;
 let userNumber;
-let userPrize = 0;
+let userPrize = startUserPrize;
+let baseMaxPrize = startBaseMaxPrize;
 let possiblePrize = 0;
-// to avoid fixing 'magic numbers' warnings in code, was pursued to hardcode prizes
-let baseMaxPrize = 100;
-let baseMidPrize = 50;
-let baseMinPrize = 25;
 // 'magic numbers' warning fix
 let basePrizeMultiplier = 2;
 
+let wantsToPlay = confirm('Do you want to play a game?');
+if (wantsToPlay === false) {
+    alert('You did not become a billionaire, but can.');
+} else {
+    while (wantsToPlay) {
+        max = max + step;
+        random = Math.floor(Math.random() * (max - min)) + min;
+        possiblePrize = baseMaxPrize;
+        wantsToPlay = false;
+        let win = false;
+        for (let i = 3; i > 0; i--) {
+            userNumber = parseInt(prompt(' Choose a roulette pocket number from 0 to ' +
+                (max - 1) + ' \n Attempts left: ' + i + ' \n Total prize: ' +
+                userPrize + '$' + '\n Possible prize: ' + possiblePrize + '$', ''));
 
-wantsToPlay = confirm('Do you want to play a game?');
-while (true) {
-    if (wantsToPlay === false) {
-        alert('You did not become a billionaire, but can.')
-        break;
-    } else {
-        max = max + 4;
-        for (var i = 3; i > 0; i--) {
-            random = Math.floor(Math.random() * (max - min)) + min;
-            if (i === 3 && random === userNumber) {
-                possiblePrize = baseMaxPrize;
-                userPrize = baseMaxPrize
-            } else if (i === 2 && random === userNumber) {
-                possiblePrize = baseMidPrize;
-                userPrize = baseMidPrize
-            } else if (i === 1 && random === userNumber) {
-                possiblePrize = baseMinPrize;
-                userPrize = baseMinPrize;
+            if (random === userNumber) {
+                win = true;
+                userPrize += possiblePrize;
+                break;
             }
-            userNumber = parseInt(prompt('Choose a roulette pocket number from 0 to ' + (max - 1) + ' \n attempts left: ' + i + ' \n Total prize: ' + userPrize + '$' + '\n possible prize: ' + possiblePrize + '$', ''));
+            // prize goes two times smaller everytime user did not guess
+            possiblePrize /= basePrizeMultiplier;
+        }
 
+        if (win) {
+            baseMaxPrize *= basePrizeMultiplier;
+            wantsToPlay = confirm('Congratulation, you won! Your prize is: ' + userPrize
+                + '$. Do you want to continue?');
+        }
 
-
-        } wantsToPlay = confirm('Congratulation, you won!  Your prize is: ' + userPrize + '$. Do you want to continue?');
-        break;
-        // else {
-        //     wantsToPlay = false;
-        //     break;
-        // }
-    }
-    if (wantsToPlay === true) {
-        baseMaxPrize *= basePrizeMultiplier;
-        baseMidPrize *= basePrizeMultiplier;
-        baseMinPrize *= basePrizeMultiplier;
-        continue;
-    } else {
-        alert('Thank you for your participation. Your prize is: ' + userPrize + '$');
-        break;
+        if (wantsToPlay === false) {
+            alert('Thank you for your participation. Your prize is: ' + userPrize + '$');
+            wantsToPlay = confirm('Do you want to play again?');
+            // reset game state
+            max = startMax;
+            userPrize = startUserPrize;
+            baseMaxPrize = startBaseMaxPrize;
+        }
     }
 }
