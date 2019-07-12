87b5ed30f5ffdffa353a4a51163cd46c1ef0d4de
Maryna <marry.pavliuk@gmail.com>
2019-07-12 15:34:39 +0300

    Fix reverse number

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -1,18 +1,14 @@
 function reverseNumber(n) {
-    let negative = n < 0;
     let digit, result = 0;
     while (n) {
         digit = n % 10;
         result = (result * 10) + digit;
         n = parseInt(n / 10);
     }
-    if (negative) {
-        result * -1;
-    }
     return result;
 }
 
-console.log(reverseNumber(-456));
+console.log(reverseNumber(456));
 
 
 
