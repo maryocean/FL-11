78bcfd69af36bc8f8729b475550951ea036cfa88
Maryna <marry.pavliuk@gmail.com>
2019-07-28 12:04:29 +0300

    Add homework 12 init commit

---
+++ FL11_HW12/FL11_HW12/homework/src/app.js
@@ -0,0 +1,9 @@
+const rootNode = document.getElementById('root');
+
+const todoItems = [
+    {isDone: false, id: 12345, description: 'Todo 1'}
+];
+
+// Your code goes here
+
+rootNode.appendChild(/* Append your list item node*/);
